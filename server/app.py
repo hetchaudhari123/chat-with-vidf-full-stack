cc09938495b64685b4cee0629de86545159e0d7b
hetchaudhari123 <hetchaudhari61@gmail.com>
2025-11-05 17:01:29 +0530

    updated

--- server/app.py
+++ server/app.py
@@ -165,7 +165,7 @@ def ask():
     # Start chat session with the current question included in history
     model = genai.GenerativeModel(
         # model_name="gemini-1.5-flash",
-        model_name="gemini-2.5-pro",
+        model_name="gemini-2.5-flash",
         generation_config=generation_config,
         system_instruction=system_instruction
     )
